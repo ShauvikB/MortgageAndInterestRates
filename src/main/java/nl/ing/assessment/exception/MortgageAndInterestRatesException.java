afbe296ce54ba27080e98c24feffc94a7642b997
shauvik <shauvik.bakshi@gmail.com>
2024-11-28 23:24:31 +0100

    Exception class to handle case when no interest rate found for maturity period provided

---
+++ src/main/java/nl/ing/assessment/exception/MortgageAndInterestRatesException.java
@@ -0,0 +1,7 @@
+package nl.ing.assessment.exception;
+
+public class MortgageAndInterestRatesException extends Exception {
+    public MortgageAndInterestRatesException(String message) {
+        super(message);
+    }
+}
